837b7c6675697e3e39133430af5e378a67f25cf6
Ashish Sharma <ashishsharma@AshsLP.local>
2024-01-08 22:17:27 -0500

    ProductService.java updated

--- src/main/java/com/springbootCRUD/springbootcrudpoc/repository/ProductRepository.java
+++ src/main/java/com/springbootCRUD/springbootcrudpoc/repository/ProductRepository.java
@@ -1,9 +1,15 @@
 package com.springbootCRUD.springbootcrudpoc.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.springbootCRUD.springbootcrudpoc.entity.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+	Product findByName(String name);
+
+	
 
 }
